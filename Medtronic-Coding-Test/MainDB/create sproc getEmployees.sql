11230192d22e2f7e7f643d5bbbd5ef22664bda85
Third Eye Software <jaydpather@gmail.com>
2023-05-25 17:32:46 -0500

    Create create sproc getEmployees.sql

---
+++ Medtronic-Coding-Test/MainDB/create sproc getEmployees.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[getEmployees]
+AS
+	select e.EmployeeId, e.FirstName, e.LastName, m.EmployeeId as ManagerId, m.FirstName as ManagerFirstName, m.LastName as ManagerLastName
+	from Employee e 
+	left join Employee m on m.Id = e.ManagerId
+	order by e.LastName, e.FirstName
+RETURN 0
