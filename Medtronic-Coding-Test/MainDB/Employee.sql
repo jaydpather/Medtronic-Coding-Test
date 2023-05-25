1be6d27ac6c25d3ef7157136643cd9ab5fb993f3
Third Eye Software <jaydpather@gmail.com>
2023-05-25 15:15:56 -0500

    sproc complete

---
+++ Medtronic-Coding-Test/MainDB/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	FirstName nvarchar(50) not null,
+	LastName nvarchar(50) not null, 
+    [ManagerId] INT NULL FOREIGN KEY REFERENCES Employee(Id)
+)
